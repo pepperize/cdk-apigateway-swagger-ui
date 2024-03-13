19899cbfc2c708e5398f8d15b88b11886613e508
Christian Ferguson <christian.ferguson@dmgmedia.co.uk>
2024-03-13 17:44:49 +0000

    feat: Replaced Node 14 runtime (no longer supported) with Node 18 (supported with no immediate deprecation date)

--- src/swagger-ui-function.ts
+++ src/swagger-ui-function.ts
@@ -17,7 +17,7 @@ export class SwaggerUiFunction extends lambda.Function {
     super(scope, id, {
       description: 'functions/src/swagger-ui.lambda.ts',
       ...props,
-      runtime: new lambda.Runtime('nodejs14.x', lambda.RuntimeFamily.NODEJS),
+      runtime: new lambda.Runtime('nodejs18.x', lambda.RuntimeFamily.NODEJS),
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../assets/functions/src/swagger-ui.lambda')),
     });
